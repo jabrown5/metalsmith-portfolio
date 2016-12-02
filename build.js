cdd27c5a79974f01a39f9df22f426d11acf809fe
Jennifer Brown <j.a.brown.5@gmail.com>
2016-12-01 23:17:48 -0600

    paginations not working

--- build.js
+++ build.js
@@ -7,6 +7,10 @@ var markdown = require('metalsmith-markdown');
 var layouts = require('metalsmith-layouts');
 var handlebars = require('handlebars');
 var collections = require('metalsmith-collections');
+
+var templates = require('metalsmith-templates');
+var pagination = require('metalsmith-pagination');
+
 var permalinks = require('metalsmith-permalinks');
 var serve = require('metalsmith-serve');
 var watch = require('metalsmith-watch');
@@ -27,11 +31,58 @@ metalsmith(__dirname)
 	        reverse: true
 	        },
 	      }))
+
+    // .use(pagination({
+    //     'collections.articles': {
+    //         perPage: 1,
+    //         first: 'index.html',
+    //         path: 'page/:num/index.html',
+    //         template: 'index.html'
+    //     }
+    // }))
+
+
+    .use(pagination({
+        'collections.articles': {
+            perPage: 2,
+            layout: 'index.html',
+
+            // template: 'partials/index.html',
+            first: 'index.html',
+            path: 'page-:num.html'
+            // filter: function (page) {
+            //     return !page.private
+            // },
+            // pageMetadata: {
+            //     title: 'Archive'
+            // }
+        }
+    }))
+    //
+    // .use(paginate({
+    //     'collections.blogposts':{
+    //         perPage: 10,
+    //         //template: 'test.hbt'
+    //         //template: 'blog-list-pages.hbt',
+    //         layout: 'blog-list-pages.hbt',
+    //         first: 'blog/index.html',
+    //         path: 'blog/page-:num.html'
+    //         //noPageOne: true
+    //         //pageMetadata: {
+    //         //    title: 'Blog'
+    //         //}
+    //     }
+    // }))
+
 	.use(markdown())
 	.use(permalinks({
 	  relative: false,
 		pattern: ':title'
 	}))
+
+    // .use(templates('handlebars'))
+
+
 	.use(layouts({
 		engine: 'handlebars',
 		directory: './layouts',
